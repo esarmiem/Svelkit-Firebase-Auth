8ee0a5891da20da10ee444308634f80ba1b30dec
elder sarmiento <eldersarmiento1@gmail.com>
2023-09-21 23:49:03 -0500

    add new h1 tailwind style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,29 @@
-<section>
-	<div class="flex flex-col items-center justify-center gap-4 mt-20 text-center">
-        <h1 class="bg-gradient-to-r from-teal-500 via-orange-500 to-yellow-500 text-transparent bg-clip-text text-4xl font-bold font-mono">Sveltkit & Firebase</h1>
-        <h2 class="bg-gradient-to-r from-teal-500 via-orange-500 to-yellow-500 text-transparent bg-clip-text text-2xl font-bold font-mono px-3">Google Login Authentication</h2>
-        <img src="/sveltekit.webp" alt="sveltekit">
-	</div>
+<script>
+	const heroTitle = 'Sveltkit & Firebase Google Authentication';
+	const heroTitleSM = 'Sveltkit & Firebase Auth';
+
+	const heroWords = heroTitle.split(' ');
+	const heroLetters = heroWords.map((word) => word.split(''));
+</script>
+
+<section
+	class="flex flex-col items-center gap-2 max-w-4xl 2xl:max-w-5xl justify-center load-hidden h-screen m-auto text-center"
+>
+	<h1
+		class="flex flex-wrap gap-x-1 text-4xl xs:text-5xl sm:text-6xl md:text-7xl lg:text-7.5xl 2xl:text-8xl font-bold font-inter !leading-min hover:cursor-default"
+	>
+		{#each heroLetters as word, wordIndex}
+			<span class="hidden lg:flex">
+				{#each word as letter, letterIndex}
+					<span
+						class="letter hover:text-transparent bg-gradient-to-br bg-clip-text [-webkit-background-clip:text] from-orange-500 to-orange-800 transition-all duration-150"
+					>
+						{letter}
+					</span>
+				{/each}
+			</span>
+		{/each}
+		<span class="lg:hidden" style="text-wrap: balance;">{heroTitleSM}</span>
+	</h1>
+	<img class="mx-auto h-80 sm:h-80 md:h-80 lg:h-80 xl:h-96" src="/sveltekit.webp" alt="sveltekit" />
 </section>
